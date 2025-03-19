1b7fa0959ed3ea5cace9bee7b337f61ec2ea5dc6
Tanner Roberts <tannerbroberts@gmail.com>
2025-03-19 18:05:48 -0500

    fix mysql -> mysql2

---
+++ routes/todos.js
@@ -0,0 +1,17 @@
+const todosRouter = require('express').Router()
+const query = require('../mysql.conf.js')
+
+todosRouter.get('/todos', (_req, res) => {
+  // get the todos from the database
+  query('SELECT * FROM todos')
+    .then((results) => {
+      // Send the results back to the client
+      res.json(results)
+    })
+    .catch((error) => {
+      console.error('Error fetching todos:', error)
+      res.status(500).json({ error: 'Internal server error' })
+    })
+})
+
+module.exports = todosRouter
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const redirectRouter = require('./routes/redirect');
 const usersRouter = require('./routes/users');
 const postDataRouter = require('./routes/postData.js');
+const todosRouter = require('./routes/todos');
 const app = express();
 
 // Muh Middlewares
@@ -13,13 +14,14 @@ app.use(express.json());
 // All paths go to this handler
 app.get('/', (_req, res) => res.send('Hello World!'));
 
-// Redirects to base path
-app.use(redirectRouter);
-
 // Gets and prints data to the console
 app.use(postDataRouter);
 
+app.use(todosRouter);
+
 // Sends user data
 app.use(usersRouter);
+
+app.use(redirectRouter);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
