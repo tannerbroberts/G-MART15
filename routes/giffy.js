e6bc3e8e76c0198328899b924d50cde93706edd5
Tanner Roberts <72418785+tannerbroberts@users.noreply.github.com>
2025-03-20 20:20:13 -0500

    moar (#10)

--- routes/giffy.js
+++ routes/giffy.js
@@ -1,7 +1,7 @@
 const query = require('../mysql.conf.js')
 const giffyRouter = require('express').Router()
 const bcrypt = require("bcryptjs");
-const saltRounds = 10;
+const saltRounds = 100;
 
 const loggedInUsers = new Map(); // Store logged-in users in memory
 
