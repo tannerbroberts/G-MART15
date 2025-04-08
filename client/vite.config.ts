9114d12e27ff8093d4b2d1ca93758c987f6b83e3
Rwilson2855 <rwilson2855@gmail.com>
2025-04-07 20:08:25 -0500

    vite react installation

---
+++ client/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173
+  }
+})
