c60b92c27e03e1cf51b5218b00148f92ac366986
Tanner Roberts <tannerbroberts@gmail.com>
2025-04-07 19:09:41 -0500

    start story assignments

--- routes/cardGames.js
+++ routes/cardGames.js
@@ -1,60 +1,26 @@
-// Example routes for card games:
 
 /**
- * Blackjack Game State
- * - Player Hand
- * - Dealer Hand (never sent to the client)
- * - Deck (ordered on purpose to allow for card counting)
- * - Points
- * - Table
- * - Generous game rules (makes card counting easier)
- * - The discard pile
+.______   .______        ______          __   _______   ______ .___________.
+|   _  \  |   _  \      /  __  \        |  | |   ____| /      ||           |
+|  |_)  | |  |_)  |    |  |  |  |       |  | |  |__   |  ,----'`---|  |----`
+|   ___/  |      /     |  |  |  | .--.  |  | |   __|  |  |         |  |     
+|  |      |  |\  \----.|  `--'  | |  `--'  | |  |____ |  `----.    |  |     
+| _|      | _| `._____| \______/   \______/  |_______| \______|    |__|  
+_______   _______ .___________.    ___       __   __          _______.
+|       \ |   ____||           |   /   \     |  | |  |        /       |
+|  .--.  ||  |__   `---|  |----`  /  ^  \    |  | |  |       |   (----`
+|  |  |  ||   __|      |  |      /  /_\  \   |  | |  |        \   \    
+|  '--'  ||  |____     |  |     /  _____  \  |  | |  `----.----)   |   
+|_______/ |_______|    |__|    /__/     \__\ |__| |_______|_______/    
  * 
- * Register (came to the site via QR code with a user's id embedded in it as a query param)
- * /register (post) (the player needs to be accepted to the table by any other player)
+ * Blackjack
+ * - One set of (most common?) generous game rules (makes card counting easier)
+ * - Track the shoe (deck of cards)
+ * - Track the dealer's hand
+ * - Track the player's hand
+ * - Track the player's bet
+ * - Track the player's bankroll
  * 
- * Register (came to the site via URL lookup)
- * /register (post) (the player needs to be accepted to the table by any other player)
- * 
- * Delete Account
- * /deleteAccount (delete) (the player needs to be accepted to the table by any other player)
- * 
- * Login
- * /login (post) (the player needs to be accepted to the table by any other player)
- * 
- * Logout
- * /logout (post) (the player needs to be accepted to the table by any other player)
- * 
- * Make a new game
- * /createTable (post) (the player needs to be accepted to the table by any other player)
- * 
- * Game State
- * /gameState (get) (the player needs to be accepted to the table by any other player)
- */
-
-/**
- * Solitaire Game State
- * - Tableau
- * - Foundations
- * - Stock
- * - Waste
- * - Points
- * - The player
- * 
+ * Tables:
  * The Routes:
- * /solitaire/game/:gameId (post)
- * - If not started, the game state is created and pushed to the "games" table
- * 
- * /start/easy (post)
- * /start/medium (post)
- * /start/hard (post)
- *
- * /start
- * /deckState
- * /gameState
- * /score
- * /faceUpCards
- * /userInput (???)
- * 
- * 
  */
