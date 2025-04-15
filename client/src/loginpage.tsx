2cf21ad2c87e839ade0795f2248ec7dc7a3c2c95
Maggie <161897473+mshaner97@users.noreply.github.com>
2025-04-15 18:20:58 -0500

    login page added, title added, button added (#34)
    Co-authored-by: Tanner Roberts <72418785+tannerbroberts@users.noreply.github.com>

---
+++ client/src/loginpage.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './loginpage.css';
+
+const LoginPage = () => {
+    return (
+      <div className="container">
+              <div className="textcontainer">
+        <p>G-MART PRESENTS</p>
+        <h1>BLACKJACK</h1>
+      </div>
+        <button className="loginbutton" onClick={() => { /* google login code */ }}>
+            Log In With Google
+        </button>
+      </div>
+    );
+  };
+
+export default LoginPage;
