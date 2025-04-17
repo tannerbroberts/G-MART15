c7ababe3a0876ef00209a81ab354243913b8bb4e
Tanner Roberts <72418785+tannerbroberts@users.noreply.github.com>
2025-04-16 20:15:57 -0500

    Fix typescript errors (#39)
    * fixed it
    
    * fixed it

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,20 +16,10 @@ app.get("/api/hello", (_req, res) => {
 });
 
 // For any other routes, send the index.html
-app.get("*", (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, "../../dist/index.html"));
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// All paths go to this handler
-// Eventually serves the app code for the Giffy app
-// app.get("/", (_req, res) => res.send("Hello World!"));
-
-// Handles CRUD opperations for the Giffy app
-// app.use("/giffy", giffyRouter);
-
-// Stops people form being idiots
-// app.use(redirectRouter); (can add back later, from previous classwork is not currently connected to anything)
